0c78e19d1a64a9da6aaec4904eb8c19d3ee15a10
webkey <webkey_i@ukr.net>
2016-03-18 16:52:53 +0300

    of 95

--- js/functions.js
+++ js/functions.js
@@ -44,16 +44,16 @@ function preloader(){
 	$spinner.fadeOut();
 	$preloader.addClass('preloader-end');
 
-	var $logo = $('.logo');
+	//var $logo = $('.logo');
 
-	if ($logo.length) {
-		var _logoPosTop = $logo.offset().top + $logo.height()/2;
-		var _logoPosLeft = $logo.offset().left + $logo.width()/2;
-
-		$('#preloader-logo').css({
-			'position': 'fixed', 'top': _logoPosTop, 'left': _logoPosLeft
-		});
-	}
+	//if ($logo.length) {
+	//	var _logoPosTop = $logo.offset().top + $logo.height()/2;
+	//	var _logoPosLeft = $logo.offset().left + $logo.width()/2;
+	//
+	//	$('#preloader-logo').css({
+	//		'position': 'fixed', 'top': _logoPosTop, 'left': _logoPosLeft
+	//	});
+	//}
 
 	$preloader.delay(350).fadeOut(500);
 }
